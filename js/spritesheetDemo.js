5c288a1d2ecf0894bb4a11acd8e3c254cde8ebd3
callmecposu <pc.cposu@gmail.com>
2025-02-25 01:19:49 -0500

    Fixed spritesheet path in spritesheetDemo

--- js/spritesheetDemo.js
+++ js/spritesheetDemo.js
@@ -6,22 +6,24 @@ const app = new PIXI.Application();
 
     document.body.appendChild(app.canvas);
 
+    const spritesheetPath =
+        window.location.origin +
+        "/" +
+        window.location.pathname.split("/").splice(-2, 1).join("/") +
+        "/images/spritesheet_demo.png";
+
     PIXI.Assets.add({
         alias: "spritesheet_demo.png",
-        src: "../images/spritesheet_demo.png",
+        src: spritesheetPath,
     });
 
     const ssTexture = await PIXI.Assets.load("spritesheet_demo.png");
 
     // create spritesheet
-    const ss = new PIXI.Spritesheet(
-        ssTexture,
-        ssData
-    );
+    const ss = new PIXI.Spritesheet(ssTexture, ssData);
 
     // generate textures asynchronously
     await ss.parse();
-
 
     // create an animated sprite
     const anim = new PIXI.AnimatedSprite(ss.animations.idle);
@@ -31,34 +33,34 @@ const app = new PIXI.Application();
     anim.play();
 
     // add event listeners
-    document.addEventListener('keydown', (e) => {
-        if (e.key == 'd' && anim.textures != ss.animations.walkEast){
-            anim.textures = ss.animations.walkEast
-            anim.animationSpeed = 0.15
-            anim.gotoAndPlay(0)
+    document.addEventListener("keydown", (e) => {
+        if (e.key == "d" && anim.textures != ss.animations.walkEast) {
+            anim.textures = ss.animations.walkEast;
+            anim.animationSpeed = 0.15;
+            anim.gotoAndPlay(0);
         }
-        if (e.key == 'a' && anim.textures != ss.animations.walkWest){
-            anim.textures = ss.animations.walkWest
-            anim.animationSpeed = 0.15
-            anim.gotoAndPlay(0)
+        if (e.key == "a" && anim.textures != ss.animations.walkWest) {
+            anim.textures = ss.animations.walkWest;
+            anim.animationSpeed = 0.15;
+            anim.gotoAndPlay(0);
         }
-        if (e.key == 's' && anim.textures != ss.animations.walkSouth){
-            anim.textures = ss.animations.walkSouth
-            anim.animationSpeed = 0.15
-            anim.gotoAndPlay(0)
+        if (e.key == "s" && anim.textures != ss.animations.walkSouth) {
+            anim.textures = ss.animations.walkSouth;
+            anim.animationSpeed = 0.15;
+            anim.gotoAndPlay(0);
         }
-        if (e.key == 'w' && anim.textures != ss.animations.walkNorth){
-            anim.textures = ss.animations.walkNorth
-            anim.animationSpeed = 0.15
-            anim.gotoAndPlay(0)
+        if (e.key == "w" && anim.textures != ss.animations.walkNorth) {
+            anim.textures = ss.animations.walkNorth;
+            anim.animationSpeed = 0.15;
+            anim.gotoAndPlay(0);
         }
-    })
+    });
 
-    document.addEventListener('keyup', (e) => {
-        anim.textures = ss.animations.idle
-        anim.animationSpeed = 0.05
-        anim.gotoAndPlay(0)
-    })
+    document.addEventListener("keyup", (e) => {
+        anim.textures = ss.animations.idle;
+        anim.animationSpeed = 0.05;
+        anim.gotoAndPlay(0);
+    });
 
     app.stage.addChild(anim);
 })();
